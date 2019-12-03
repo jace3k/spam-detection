fd62634065e678f9b912b6beb705be3c89f9b6b6
Jaco <jace3k@gmail.com>
2019-12-03 21:42:49 +0100

    implemented sms spam prediction

--- main.py
+++ main.py
@@ -1,69 +1,68 @@
-"""
-Prosty eksperyment klasyfikacji.
-"""
 import numpy as np
 import pandas as pd
-from sklearn import base
 from sklearn import metrics
 from sklearn import model_selection
-from sklearn import neighbors, naive_bayes, tree, svm, neural_network
+from sklearn import neighbors
 from sklearn import preprocessing
+from sklearn.decomposition import PCA
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-LABELS_LAST = True
-LABELS_FIRST = False
-
-
-def get_datasets():
-    return [
-        (pd.read_csv('datasets/spam_email.data'), LABELS_LAST),
-        (pd.read_csv('datasets/spam_sms.csv', encoding='latin-1'), LABELS_FIRST)
-    ]
+FEATURES = 100
+FOLDS = 10
+DATASET_PATH = 'datasets/spam_sms.csv'
 
 
 def get_values_and_labels(ds):
-    labels_last = ds[1]
-    data = ds[0]
-    if labels_last:
-        return data.values[:, :-2], data.values[:, -1]
-    return data.values[:, 1:], data.values[:, 0]
+    return ds.values[:, 1], ds.values[:, 0]
 
 
-datasets = get_datasets()
-
-clfs = {
-    "kNN": neighbors.KNeighborsClassifier(),
-    "GNB": naive_bayes.GaussianNB(),
-    "DTC": tree.DecisionTreeClassifier(),
-    "SVC": svm.SVC(gamma='scale'),
-    "MLP": neural_network.MLPClassifier(),
-}
-
-k = 5
-
-scores = np.zeros((len(datasets), len(clfs), k))
-
-for did, dataset in enumerate(datasets):
+def preprocess_data(dataset):
     X, y = get_values_and_labels(dataset)
+    print('Dataset shape: {}'.format(X.shape))
+    print('Vectorizing data..')
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(X, y).toarray()
+    print('Vectorized shape: {}'.format(X.shape))
 
+    print('Feature reduction..')
+    pca = PCA(n_components=FEATURES, copy=False)
+    X = pca.fit_transform(X)
+
+    print('Scaling..')
     scaler = preprocessing.StandardScaler()
-    scaler.fit(X)
-    scaled_X = scaler.transform(X)
+    X = scaler.fit_transform(X)
 
+    print('Shape: {}'.format(X.shape))
+
+    return X, y
+
+
+def predict(X, y, clf, k=6):
     skf = model_selection.StratifiedKFold(n_splits=k)
-    for fold, (train, test) in enumerate(skf.split(scaled_X, y)):
-        X_train, X_test = scaled_X[train], scaled_X[test]
+    scores = np.zeros(k)
+    for fold, (train, test) in enumerate(skf.split(X, y)):
+        print('Fold: {}/{}'.format(fold + 1, k))
+        X_train, X_test = X[train], X[test]
         y_train, y_test = y[train], y[test]
+        print('-- fitting..')
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
 
-        for cid, clfn in enumerate(clfs):
-            clf = base.clone(clfs[clfn])
+        score = metrics.accuracy_score(y_test, y_pred)
+        print('-- accuracy: {}'.format(score))
+        scores[fold] = score
 
-            clf.fit(X_train, y_train)
-
-            y_pred = clf.predict(X_test)
-
-            score = metrics.accuracy_score(y_test, y_pred)
-
-            scores[did, cid, fold] = score
+    return np.mean(scores)
 
 
-print(np.mean(scores, axis=2))
+def main():
+    knn = neighbors.KNeighborsClassifier()
+    dataset = pd.read_csv(DATASET_PATH, encoding='latin-1')
+
+    X, y = preprocess_data(dataset)
+    result = predict(X, y, knn, FOLDS)
+    print('Accuracy: {:2f}%'.format(result * 100))
+
+
+if __name__ == '__main__':
+    main()
